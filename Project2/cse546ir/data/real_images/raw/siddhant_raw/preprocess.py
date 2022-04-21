c5e0a6fc542dac6c42e73876ff6d1f017a486da1
Chirag Vartak <vartak.chirag.8@gmail.com>
2022-04-21 10:18:43 -0700

    Train a better with images provided by Siddhant

--- Project2/cse546ir/data/real_images/raw/siddhant_raw/preprocess.py
+++ Project2/cse546ir/data/real_images/raw/siddhant_raw/preprocess.py
@@ -4,17 +4,17 @@ import os
 # subprocess.run(["ffmpeg", "-i", "1.jpeg", "-filter:v", "crop=1200:1200:100:150", "1.png"])
 # ffmpeg -i 3.png -filter:v "crop=1200:1200:100:150" 3cropped.png
 def cropAndConvertToPng():
-    for jpeg in os.listdir(""):
+    for jpeg in os.listdir("."):
         base, ext = jpeg.split(".")
-        if ext == "jpg":
-            subprocess.run(["ffmpeg", "-i", jpeg, "-filter:v", "crop=1200:1200:550:0", f"{base}.png"])
+        if ext == "jpg" and base.isnumeric() and 26<=int(base)<=35:
+            subprocess.run(["ffmpeg", "-i", jpeg, "-filter:v", "crop=600:600:300:0", f"{base}.png"])
 
 # Convert to 160x160
 # ffmpeg -i 3.png -vf scale=160:160 output_160.png
 def convertTo160x160():
-    for png in os.listdir(""):
+    for png in os.listdir("."):
         base, ext = png.split(".")
-        if ext == "png":
+        if ext == "png" and base.isnumeric() and 26<=int(base)<=35:
             subprocess.run(["ffmpeg", "-i", png, "-vf", "scale=160:160", f"{base}_160.png"])
 
 if __name__ == "__main__":
