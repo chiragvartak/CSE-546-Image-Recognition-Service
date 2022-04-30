87d0ada5226f641e0cae13aa5da24d9341a1d451
Siddhant Srivastava <sidsrivast42@gmail.com>
2022-04-30 13:58:26 -0700

    face recognition and push response to sqs

--- Project2/cse546ir/cse546ir.py
+++ Project2/cse546ir/cse546ir.py
@@ -4,6 +4,7 @@ import boto3
 import os
 import subprocess
 import time
+import eval_face_recognition2
 
 print('Loading function')
 
@@ -11,8 +12,11 @@ session = boto3.Session(
     aws_access_key_id='',
     aws_secret_access_key='',
 )
-s3 = session.client('s3')
 
+s3 = session.client('s3')
+dynamodb = session.client('dynamodb', region_name='us-east-1')
+sqs = session.client('sqs', region_name='us-east-1')
+queue = sqs.get_queue_by_name(QueueName='cc-project2-response-queue')
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event))
@@ -28,13 +32,31 @@ def lambda_handler(event, context):
     
     start = time.time()
     # process_response = subprocess.run(['ffmpeg', '-sseof', '5', '-i', save_file, '-vframes', '1', '/tmp/end.jpeg'], capture_output=True)
-    process_response = subprocess.run(['ffmpeg', '-i', save_file, '-r', '2', frame_save_regex], capture_output=True)
+    process_response = subprocess.run(['ffmpeg', '-i', save_file, '-vf', 'fps=1/0.5', '/tmp/img%03d.jpeg'], capture_output=True)
     print('TIME: %s' % str(time.time()-start))
+
+    # ls_process = subprocess.Popen(["ls", "-l", '/tmp'], stdout=subprocess.PIPE)
+    # out, err = ls_process.communicate()
+    # print(out)
 
     for frame_filename in os.listdir('/tmp'):
         if frame_filename.endswith('.jpeg'):
-            print('UPLOADING FRAME %s' % frame_filename)
-            # response = s3.upload_file(os.path.join('/tmp', frame_filename), 'cc-project2-frame-bucket', frame_filename)
-            print('FRAME %s UPLOADED' % frame_filename)
+            print(frame_filename)
+            result = eval_face_recognition2.evalImage(frame_filename)
+            print('INFO: FACE RECOGNIZED AS: %s' % result)
 
+            db_item = dynamodb.get_item(
+                TableName='cc-project2-info-table',
+                Key={
+                    'name': {'S': 'siddhant'}
+                }
+            )['Item']
+            print('INFO: ITEM FETCHED FROM DYNAMO DB: %s' % db_item)
+
+            sqs_response = queue.send_message(
+                QueueUrl=queue.url,
+                MessageAttributes={},
+                MessageBody=json.dumps(response['Item'])
+            )
+            print('INFO: SQS RESPONSE: %s' % (sqs_response))
     return True
