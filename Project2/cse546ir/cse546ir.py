02ff383124940c28d37aee6d5d0b980469fb581f
Chirag Vartak <vartak.chirag.8@gmail.com>
2022-04-30 17:27:40 -0700

    Clean up code

--- Project2/cse546ir/cse546ir.py
+++ Project2/cse546ir/cse546ir.py
@@ -1,52 +1,62 @@
-# Set environment variables even before importing the module - you never know which modules use which env variables
-import os
-os.environ["TORCH_HOME"] = "/tmp/PyTorchHome"
-
 import json
 import urllib.parse
 import boto3
 import subprocess
 import time
+import os
+
+startLoadingTime = time.time()
+print('Loading function ...')
+os.environ["TORCH_HOME"] = "/tmp/PyTorchHome"  # Set environment variables even before importing the modules - you
+                                               # never know which modules use which env variables
 import eval_face_recognition2
 
-print('Loading function')
+# Import private configs, if the private_config.py file exists
+aws_access_key_id = ''
+aws_secret_access_key = ''
+try:
+    import private_config
+    aws_access_key_id = private_config.ACCESS_KEY_ID
+    aws_secret_access_key = private_config.ACCESS_KEY_SECRET
+except ModuleNotFoundError:
+    pass
 
-session = boto3.Session(
-    aws_access_key_id='',
-    aws_secret_access_key='',
-)
-
+# Initialize AWS resources
+session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 s3 = session.client('s3')
 dynamodb = session.client('dynamodb', region_name='us-east-1')
 sqs = session.client('sqs', region_name='us-east-1')
 queue = sqs.get_queue_by_name(QueueName='cc-project2-response-queue')
+print("... loaded function. Took %.3f secs" % (time.time()-startLoadingTime))
+
 
 def lambda_handler(event, context):
+    startLambdaTime = time.time()
     print("Received event: " + json.dumps(event))
 
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    print("INPUT BUCKET AND KEY:  { Bucket: '%s', Key: '%s' }" % (bucket, key))
+    print("Input bucket and key: { Bucket: '%s', Key: '%s' }" % (bucket, key))
 
     save_file = '/tmp/%s' % key
-    frame_save_regex = '/tmp/frame_%s_%s.jpeg' % (key.split('.')[0].split('_')[1], '%02d')
+    # frame_save_regex = '/tmp/frame_%s_%s.jpeg' % (key.split('.')[0].split('_')[1], '%02d')
+    startVideoDownloadTime = time.time()
     s3.download_file(bucket, key, save_file)
+    print("Time taken to download video: %.3f" % (time.time()-startVideoDownloadTime))
     
-    start = time.time()
+    startExtractFramesTime = time.time()
     # process_response = subprocess.run(['ffmpeg', '-sseof', '5', '-i', save_file, '-vframes', '1', '/tmp/end.jpeg'], capture_output=True)
     process_response = subprocess.run(['ffmpeg', '-i', save_file, '-vf', 'fps=1/0.5', '/tmp/img%03d.jpeg'], capture_output=True)
-    print('TIME: %s' % str(time.time()-start))
-
-    # ls_process = subprocess.Popen(["ls", "-l", '/tmp'], stdout=subprocess.PIPE)
-    # out, err = ls_process.communicate()
-    # print(out)
+    print('Time taken to extract frames: %.3f' % (time.time()-startExtractFramesTime))
 
     for frame_filename in os.listdir('/tmp'):
         if frame_filename.endswith('.jpeg'):
             print(frame_filename)
+            startImageRecognitionTime = time.time()
             result = eval_face_recognition2.evalImage("/tmp/" + frame_filename)
-            print('INFO: FACE RECOGNIZED AS: %s' % result)
+            print("Time taken for image recog: %.3f" % (time.time()-startImageRecognitionTime))
+            print('Face recognized: %s' % result)
 
             db_item = dynamodb.get_item(
                 TableName='cc-project2-info-table',
@@ -54,12 +64,13 @@ def lambda_handler(event, context):
                     'name': {'S': 'siddhant'}
                 }
             )['Item']
-            print('INFO: ITEM FETCHED FROM DYNAMO DB: %s' % db_item)
+            print('Item fetched from DynamoDb: %s' % db_item)
 
             sqs_response = queue.send_message(
                 QueueUrl=queue.url,
                 MessageAttributes={},
                 MessageBody=json.dumps(response['Item'])
             )
-            print('INFO: SQS RESPONSE: %s' % (sqs_response))
+            print('SQS Response: %s' % sqs_response)
+    print("Time taken for lambda (excluding loading): %.3f" % (time.time()-startLambdaTime))
     return True
--- Project2/cse546ir/eval_face_recognition2.py
+++ Project2/cse546ir/eval_face_recognition2.py
@@ -11,38 +11,26 @@ model_path = "checkpoint/model_vggface2_best.pth"
 
 # read labels
 with open(labels_dir) as f:
-     labels = json.load(f)
-# print(f"labels: {labels}")
+    labels = json.load(f)
 
 device = torch.device('cpu')
 model = build_custom_model.build_model(len(labels)).to(device)
 model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'))['model'])
 model.eval()
-# print(f"Best accuracy of the loaded model: {torch.load(model_path, map_location=torch.device('cpu'))['best_acc']}")
+
 
 def evalImage(img_path: str) -> str:
+    img = Image.open(img_path)
+    img_tensor = transforms.ToTensor()(img).unsqueeze_(0).to(device)
+    outputs = model(img_tensor)
+    _, predicted = torch.max(outputs.data, 1)
+    result = labels[np.array(predicted.cpu())[0]]
+    return result
 
-     # parser = argparse.ArgumentParser(description='Evaluate your customized face recognition model')
-     # parser.add_argument('--img_path', type=str, default="./data/test_me/val/angelina_jolie/1.png", help='the path of the dataset')
-     # args = parser.parse_args()
-     # img_path = args.img_path
-
-     img = Image.open(img_path)
-     img_tensor = transforms.ToTensor()(img).unsqueeze_(0).to(device)
-     outputs = model(img_tensor)
-     _, predicted = torch.max(outputs.data, 1)
-     result = labels[np.array(predicted.cpu())[0]]
-     # print(predicted.data, result)
-
-     # img_name = img_path.split("/")[-1]
-     # img_and_result = f"({img_name}, {result})"
-     # print(f"Image and its recognition result is: {img_and_result}")
-
-     return result
 
 if __name__ == "__main__":
-     start = time()
-     result = evalImage("data/real_images/val/chirag/9_160.png")
-     end = time()
-     timeTaken = "%.2f" % (end - start)
-     print("Result = %s, time taken = %ss" % (result, timeTaken))
+    start = time()
+    result = evalImage("data/real_images/val/chirag/9_160.png")
+    end = time()
+    timeTaken = "%.2f" % (end - start)
+    print("Result = %s, time taken = %ss" % (result, timeTaken))
